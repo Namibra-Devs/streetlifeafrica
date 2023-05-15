a0ed2a8bfac223fa9e27f6716da4e66d39d269bf
Ibrahim Sulemana <ibeevpersie@gmail.com>
2023-05-15 11:17:37 +0000

    About section done

--- src/js/main.js
+++ src/js/main.js
@@ -35,20 +35,31 @@ window.document.addEventListener('DOMContentLoaded', () => {
 
 
 //   Display the about text
-    const about = document.querySelector('.show-aboout');
+    const about = document.querySelectorAll('.show-aboout');
     const aboutText = document.querySelectorAll('.about-text');
 
-    // Show the about text when clicking on the about button and hide the other text
-    // about.addEventListener('click', () => {
-    //     aboutText.forEach((text) => {
-    //         text.classList.toggle('hidden');
-    //     });
-    // });
-
-    // Show the about text when clicking on the about button and hide the other text
-    for (let i = 0; i < aboutText.length; i++) {
-        about.addEventListener('click', () => {
+    for(let i = 0; i < about.length; i++) {
+        about[i].addEventListener('click', () => {
             aboutText[i].classList.toggle('hidden');
+            aboutText[i].style = 'transition: all 0.5s ease-in-out;'
+
+            // rotate arrow
+            if(aboutText[i].classList.contains('hidden')) {
+                about[i].style = 'transform: rotate(0deg); transition: transform 0.5s ease-in-out;'
+            } else {
+                about[i].style = 'transform: rotate(-180deg); transition: transform 0.5s ease-in-out;'
+
+            }
+
         });
     }
+
+    // close menu when clicking outside
+    window.addEventListener('click', (e) => {
+
+        if (e.target !== aboutText[i] && e.target !== about[i]) {
+            aboutText[i].classList.add('hidden');
+        }
+    }
+    );
 });
